2f0269e22a22e7f87e6e317c50a8ab5fc06e9b33
root <root@ALGGDP020.ALIGNET.com>
2019-10-10 14:26:42 -0500

    mi primer commit

---
+++ test1.py
@@ -0,0 +1,8 @@
+intento = int(5)
+
+if(intento==3):
+    print("Numero de intentos de cargos es 3")
+elif(intento==2):
+    print("Numero de intentos de cargos es 2")
+else:
+    print("Numero de intentos de cargos es: %s, by %s" % (str(intento), 'Vilia'))
